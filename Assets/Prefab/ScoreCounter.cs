bb4a05b1192757c2dbdda5339ab2225b0404d539
Treehatshrimp <jasonjiang000@gmail.com>
2026-02-09 18:36:07 -0600

    Finished with game

---
+++ Assets/Prefab/ScoreCounter.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; //lets use Text
+
+public class ScoreCounter : MonoBehaviour
+{
+    [Header("Dynamic")]
+    public int score = 0;
+    private Text uiText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = score.ToString("#,0"); //this 0 is zero
+    }
+}
